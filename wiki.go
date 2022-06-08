8824fcb6ecd9e00c669e6357183216222eb1f8d8
Nazarov Bahodur <nazarovbahodur1999@gmail.com>
2022-06-08 11:29:43 +0500

    added Page struct, func loadPage

--- wiki.go
+++ wiki.go
@@ -5,3 +5,18 @@ import (
 	"io/ioutil"
 )
 
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	fn := p.Title + ".txt"
+	return ioutil.WriteFile(fn, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+	fn := title + ".txt"
+	body, _ := ioutil.ReadFile(fn)
+	return &Page{Title: title, Body: body}
+}
